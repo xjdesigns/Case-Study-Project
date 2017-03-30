ffa23eb6ee07afbbe9ffaf3cc67c76c911a734ce
Jason Jacobson <jdesigns.jason@gmail.com>
2017-03-30 10:43:38 -0500

    image carousel working, transitions, added primary to start of alt array

--- js/carousel/img-carousel.component.js
+++ js/carousel/img-carousel.component.js
@@ -11,18 +11,47 @@ img.component('imgCarousel', {
 function ImageCarousel() {
   var crsl = this;
   var imgs = {};
-  var currentSlide = 0;
+  var imgWidth = 63; // match the image widths, we know from our styling
+  var slide = 0;
+  crsl.currentSlide = 0;
+  crsl.offset = 0;
   crsl.currentImg;
   crsl.altImgs;
+  crsl.length;
 
   this.$onInit = function() {
     imgs = crsl.images[0];
     crsl.currentImg = imgs.PrimaryImage[0].image;
-    crsl.altImgs = imgs.AlternateImages;
-    console.warn('this', imgs, this);
+    crsl.altImgs = this.addPrimaryToAlt(imgs.AlternateImages, imgs.PrimaryImage[0]);
+    crsl.length = crsl.altImgs.length;
   }
 
-  this.nextImage = function(idx) {
-    console.warn('Image IDX::', idx);
+  this.addPrimaryToAlt = function(o, n) {
+    o.unshift(n);
+    return o;
+  }
+
+  this.selectImage = function(idx) {
+    crsl.currentSlide = idx;
+    crsl.currentImg = crsl.altImgs[crsl.currentSlide].image;
+  }
+
+  this.nextImage = function() {
+    if (slide === (crsl.length - 3)) {
+      return;
+    }
+    crsl.offset = crsl.offset + imgWidth;
+    slide++;
+  }
+  this.previousImage = function() {
+    if (slide === 0) {
+      return;
+    }
+    crsl.offset = crsl.offset - imgWidth;
+    slide--;
+  }
+
+  this.openZoomModal = function() {
+    alert('ZOOM ZOOM');
   }
 }
