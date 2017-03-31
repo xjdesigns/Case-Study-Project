b88c694b2ec3be56113dd1e18f3769c96d02036a
Jason Jacobson <jdesigns.jason@gmail.com>
2017-03-31 11:00:08 -0500

    fixed date to match designs, carousel style fix

--- js/core/core.controller.js
+++ js/core/core.controller.js
@@ -43,6 +43,10 @@ core.controller('myRetailController', ['$scope', 'httpService', '$sce', function
   $scope.toDate = function(date) {
     var opts = {year: 'numeric', month: 'short'};
     var d = new Date(date);
-    return d.toLocaleDateString('en-US', opts);
+    var locale = d.toLocaleDateString('en-US', opts);
+    var month = locale.slice(0,3);
+    var day = d.getDate();
+    var f = locale.replace(/^.{0,3}/, month + ' ' + day + ',');
+    return f;
   }
 }]);
